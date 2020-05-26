bc5b793e31cdc5ae1cece5af7951cdc6fb5f4fe4
phongnsfx02725 <64692367+phongnsfx02725@users.noreply.github.com>
2020-05-26 23:57:35 +0800

    Update removeDuplicates
    int arraySize = array.size();
    for (int i = 0; i < arraySize - 1; i++) {
            if (array.get(i).equals(array.get(i + 1))) {
                array.remove(i + 1);
                i--;
                arraySize--;
            }
        }

--- chapter-10/removeDuplicates.java
+++ chapter-10/removeDuplicates.java
@@ -4,11 +4,13 @@
 */
 
 public static void removeDuplicates(ArrayList<String> array) {
-
-    for (int i = 0; i < array.size() - 1; i++) {
+    
+    int arraySize = array.size();
+    for (int i = 0; i < arraySize - 1; i++) {
         if (array.get(i).equals(array.get(i + 1))) {
             array.remove(i + 1);
             i--;
+            arraySize--;
         }
     }
     
